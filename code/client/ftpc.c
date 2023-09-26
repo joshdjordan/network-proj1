ea8b4d781e32556d074a52016cc29f4f2381eed1
Josh Jordan <joshd.jordan@yahoo.com>
2023-09-26 09:21:41 -0600

    completed

--- code/client/ftpc.c
+++ code/client/ftpc.c
@@ -1,3 +1,10 @@
+/*
+Name: Josh Jordan
+Date: 9-16-2023
+Description: project 1 client side of TCP connection program
+NOTES:
+*/
+
 #include <string.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -8,7 +15,6 @@
 #include <stdbool.h>
 
 #define TOTAL_SIZE 100
-#define MAX_FILENAME_SIZE 100
 
 int getFileLength(FILE *fp)
 {
@@ -23,44 +29,40 @@ int main(int argc, char *argv[])
     // declarations
     int socketDescriptor,portNumber;
     struct sockaddr_in server_address;
-    char fileName[MAX_FILENAME_SIZE],outputFileName[MAX_FILENAME_SIZE];
+    char fileName[20],outputFileName[20];
     char serverIP[29];
     int returnCode = 0,totalBytesRead = 0,totalBytesWritten = 0;
     int file_size = 0;
     FILE *inputFile,*outputFile;
 
-    if (argc < 3)
+    if(argc < 3)
     {
         printf("Usage: client <ipaddr> <port>\n");
         exit(1);
     }
-
     socketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
     portNumber = atoi(argv[2]);
-    strcpy(serverIP, argv[1]);
+    strcpy(serverIP,argv[1]);
 
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(portNumber);
     server_address.sin_addr.s_addr = inet_addr(serverIP);
 
-    if (connect(socketDescriptor, (struct sockaddr *) &server_address, sizeof(struct sockaddr_in)) < 0)
+    if(connect(socketDescriptor,(struct sockaddr *) &server_address,sizeof(struct sockaddr_in)) < 0)
     {
         close(socketDescriptor);
         perror("Error connecting stream socket");
         exit(1);
     }
 
-    while (true)
+    while(true)
     {
         printf("What is the name of the file you would like to send?\n> ");
         scanf("%s",fileName);
-        if (strcmp(fileName,"DONE") == 0)
+        if(strcmp(fileName,"DONE") == 0)
         {
             break;
         }
-        printf("What do you want to name the output file?\n> ");
-        scanf("%s",outputFileName);
-
         // Open the file inside the loop for each file
         inputFile = fopen(fileName,"rb");
         if(inputFile == NULL)
@@ -72,28 +74,28 @@ int main(int argc, char *argv[])
         file_size = getFileLength(inputFile);
 
         int fileName_size = strlen(fileName);
-        returnCode = write(socketDescriptor, &fileName_size, sizeof(int));
-        if (returnCode < 0)
+	    int convertedFileNameSize = htonl(fileName_size);// DMO
+        returnCode = write(socketDescriptor,&convertedFileNameSize,sizeof(int));//DMO
+        if(returnCode < 0)
         {
             perror("write");
             fclose(inputFile); // Close the file if write fails
             exit(1);
         }
-        printf("Sent filename size: %d bytes\n", fileName_size);
+        printf("Sent filename size: %d bytes\n",fileName_size);
 
-        returnCode = write(socketDescriptor, fileName, fileName_size);
-        if (returnCode < 0)
+        returnCode = write(socketDescriptor,fileName,fileName_size);
+        if(returnCode < 0)
         {
             perror("write");
             fclose(inputFile); // Close the file if write fails
             exit(1);
         }
-        printf("Sent file name: '%s'\n", fileName);
+        printf("Sent file name: '%s'\n",fileName);
 
         int file_size_n = htonl(file_size);
-        printf("file size: %d bytes\n",file_size);
-        returnCode = write(socketDescriptor, &file_size, sizeof(int));
-        if (returnCode < 0)
+        returnCode = write(socketDescriptor,&file_size_n,sizeof(int)); // DMO
+        if(returnCode < 0)
         {
             perror("write");
             fclose(inputFile);
@@ -105,15 +107,15 @@ int main(int argc, char *argv[])
          * SENDING FILE *
          ****************/
 
-        char buffer[TOTAL_SIZE];
+        char buffer[20];
         int bytes_read = 0;
-        while ((bytes_read = fread(buffer, 1, TOTAL_SIZE, inputFile)) > 0)
+        while((bytes_read = fread(buffer,1,20,inputFile)) > 0)
         {
             int total_sent_bytes = 0;
-            while (total_sent_bytes < bytes_read)
+            while(total_sent_bytes < bytes_read)
             {
-                int sent_bytes = write(socketDescriptor, buffer + total_sent_bytes, bytes_read - total_sent_bytes);
-                if (sent_bytes < 0)
+                int sent_bytes = write(socketDescriptor,buffer + total_sent_bytes,bytes_read - total_sent_bytes);
+                if(sent_bytes < 0)
                 {
                     perror("Error sending file data");
                     fclose(inputFile); // Close the file if write fails
@@ -122,12 +124,8 @@ int main(int argc, char *argv[])
                 total_sent_bytes += sent_bytes;
             }
         }
-        printf("\nSent the file.\n");
-
-        // Close the file after sending
         fclose(inputFile);
     }
-
     close(socketDescriptor);
 
     return 0;
--- code/server/ftps.c
+++ code/server/ftps.c
@@ -12,6 +12,39 @@ Notes:
 #include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
+
+#define BUFFSIZE 1000
+
+int readfile(FILE *outputFile,int connected_socketDescriptor,int file_size)
+{
+    unsigned char buffer[BUFFSIZE];
+    int totalBytesRead = 0, totalBytesWritten = 0;
+
+    while(totalBytesRead < file_size)
+    {
+        int bytesRead = read(connected_socketDescriptor, buffer, sizeof(buffer));
+        if(bytesRead < 0)
+        {
+            perror("Error reading from socket");
+            return -1;
+        }
+        if(bytesRead == 0)
+        {
+            // Connection closed or no more data to read
+            break;
+        }
+        int bytesToWrite = bytesRead;
+        int bytesWritten = fwrite(buffer, 1, bytesToWrite, outputFile);
+        if(bytesWritten < 0)
+        {
+            perror("Error writing to file");
+            return -1;
+        }
+        totalBytesRead += bytesRead;
+        totalBytesWritten += bytesWritten;
+    }
+}
 
 int main(int argc,char *argv[])
 {
@@ -22,11 +55,12 @@ int main(int argc,char *argv[])
     // declarations
     int socketDescriptor,connected_socketDescriptor;
     int returnCode,portNumber,fileNameSize,file_size;
-    int totalBytes = 0;
+    int ttlBytes = 0,ttlBytesRecv = 0,ttlBytesWritten = 0;
     struct sockaddr_in server_address;
     struct sockaddr_in from_address;
     socklen_t fromLength = sizeof(struct sockaddr_in);
     char buffer[100];
+    FILE *inputFile,*outputFile;
 
     if(argc < 2)
     {
@@ -48,30 +82,56 @@ int main(int argc,char *argv[])
     }
     listen(socketDescriptor,5);
     connected_socketDescriptor = accept(socketDescriptor,(struct sockaddr *) &from_address, &fromLength);
-    memset(buffer,0,100);
-
-    returnCode = read(connected_socketDescriptor,&fileNameSize,sizeof(int));
-    printf("received file name size: %d bytes\n",returnCode);
-
-    returnCode = read(connected_socketDescriptor,&buffer,100);
-    if(returnCode <= 0)
+    
+    while(true)
     {
-        perror("read");
-        exit(1);
+        memset(buffer,0,100);
+
+        if(strcmp(buffer,"DONE") == 0)
+        {
+            printf("exiting...");
+            break;
+        }
+        returnCode = read(connected_socketDescriptor,&fileNameSize,sizeof(int));
+        printf("received file name size: %d bytes\n",returnCode);
+
+        int convertedFileNameSize = ntohl(fileNameSize); // DMO
+        returnCode = read(connected_socketDescriptor,buffer,convertedFileNameSize); 
+        if(returnCode <= 0)
+        {
+            perror("read");
+            exit(1);
+        }
+        printf("Received file name: '%s'\n",buffer);
+
+        int file_size_n = 0;
+        returnCode = read(connected_socketDescriptor, &file_size_n, sizeof(int));
+        if(returnCode < 0)
+        {
+            perror("read");
+            exit(1);
+        }
+        file_size = ntohl(file_size_n);  // Convert back to host byte order
+        printf("Received size of file: %d byte\n", file_size);
+
+        outputFile = fopen(buffer,"wb");
+        if(outputFile == NULL)
+        {
+            perror("File open failed");
+            exit(1);
+        }
+
+        if(readfile(outputFile,connected_socketDescriptor,file_size) < 0)
+        {
+            fclose(outputFile);
+            close(connected_socketDescriptor);
+            close(socketDescriptor);
+            exit(1);
+        }
+        fclose(outputFile);
     }
-    printf("Received file name: '%s'\n",buffer);
-
-    printf("Server is waiting for the file size...\n");
-
-    int file_size_n = 0;
-    returnCode = read(connected_socketDescriptor, &file_size_n, sizeof(int));
-    if(returnCode < 0)
-    {
-        perror("read");
-        exit(1);
-    }
-    file_size = ntohl(file_size_n);  // Convert back to host byte order
-    printf("Received size of file: %d bytes\n", file_size);
-
+    close(connected_socketDescriptor);
+    close(socketDescriptor);
+    
     return 0;
 }
